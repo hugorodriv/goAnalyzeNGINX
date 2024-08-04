f03b2f80af6e990462a6f8ef5bfe3b416dd76eda
Hugo <114680022+hugorodriv@users.noreply.github.com>
2024-08-04 13:57:38 +0200

    SSE working

--- exec/instant/instant.go
+++ exec/instant/instant.go
@@ -1,7 +1,6 @@
 // TODO:
 //
 //	auto locate a .csv file in data/ (also for historic)
-//	separate shared functionality into a standalone shared package
 package main
 
 import (
@@ -13,25 +12,40 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
-var inputFile = "./data/fakeLogs.log" // nginx log file
+var (
+	reqLimit  = 100
+	inputFile = "./data/fakeLogs.log" // nginx log file
+)
 
-func listen(database shared.IpDatabase) {
-	// SSE Config
-
+func listen(database shared.IpDatabase, w http.ResponseWriter, f http.Flusher, ctx <-chan struct{}) {
 	// Parse File
 	cmd := exec.Command("tail", "--lines", "1", "--retry", "--follow", inputFile)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer stdout.Close()
+
 	scanner := bufio.NewScanner(stdout)
+
+	counter := 1
 	go func() {
 		for scanner.Scan() {
-			ip := strings.Fields(scanner.Text())[0]
-			processIP(ip, database)
+			if counter > reqLimit {
+				return
+			}
+			select {
+			case <-ctx:
+				return
+			default:
+				text := scanner.Text()
+				fields := strings.Fields(text)
+				fmt.Fprintf(w, "data: %s\n\n", shared.FindCountry(fields[0], database))
+				f.Flush()
+			}
+			counter++
 		}
 	}()
 
@@ -48,40 +62,30 @@ func listen(database shared.IpDatabase) {
 	}
 }
 
-func processIP(ip string, database shared.IpDatabase) {
-	country := shared.FindCountry(ip, database)
-	fmt.Println(country)
-	http.HandleFunc("/events", eventsHandler)
-	// http.ListenAndServe(":8080", nil)
-}
-
-func eventsHandler(w http.ResponseWriter, r *http.Request) {
-	// Set CORS headers to allow all origins. You may want to restrict this to specific origins in a production environment.
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
-
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	for i := 0; i < 10; i++ {
-		fmt.Fprintf(w, "data: %s\n\n", fmt.Sprintf("Event %d", i))
-		time.Sleep(2 * time.Second)
-		w.(http.Flusher).Flush()
-	}
-
-	// Simulate closing the connection
-	closeNotify := w.(http.CloseNotifier).CloseNotify()
-	<-closeNotify
-}
-
 func main() {
 	// Config SSE
-	// println(shared.IpToInt("127.0.0.1"))
 	database, err := shared.ParseDatabase()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
-	listen(database)
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			fmt.Println(ok)
+		}
+
+		ctx := r.Context().Done()
+		listen(database, w, flusher, ctx)
+		<-r.Context().Done()
+	})
+
+	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
